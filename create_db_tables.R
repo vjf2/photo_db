7110c82fb9b2c3987a5289c97675fe4602e830c4
vjf2 <foroughirad@gmail.com>
2020-11-12 23:34:20 -0500

    First commit

---
+++ create_db_tables.R
@@ -0,0 +1,21 @@
+## Dolphin Photo Database Tester Tables
+
+photo <- data.frame(photo_id=, 
+                    photo_date=, 
+                    photo_time=,
+                    photo_type=,
+                    photo_nbr=,
+                    platform=, 
+                    photographer, 
+                    photographer_detail=,
+                    camera_id=,
+                    camera_detail=,
+                    photo_directory=,
+                    photo_filename=,
+                    photo_quality=,
+                    dolphin_present=,
+                    notes=)
+
+photo_tag <- data.frame(photo_id=, 
+                        tag_type=,
+                        tag_value)
